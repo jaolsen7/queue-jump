f58135018122e3b1b6ad245dc530b81c48678e76
chrismuto <chrismuto@gmail.com>
2022-03-11 14:21:58 -0800

    added modal to front page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,43 @@
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../util/auth";
-import "./Navbar.css";
+import { Modal, Button } from "react-bootstrap"
+import { useState } from "react";
+// import "./Navbar.css";
 
 export default function Navbar() {
   const { isLoggedIn, logout } = useAuth();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <nav className="navbar">
-      <NavLink to="/" className="navbar-link">
+    <nav className="navbar w-100">
+<img src={require('./QueueJumpLogo.png')} alt={"Logo"} className="w-100 d-block"/>
+
+
+return (
+  <>
+    <Button variant="primary" onClick={handleShow}>
+      Launch demo modal
+    </Button>
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
+);
+}
+{/* <NavLink to="/" className="navbar-link">
         Home
       </NavLink>
       {isLoggedIn ? (
@@ -27,7 +58,7 @@ export default function Navbar() {
             Signup
           </NavLink>
         </>
-      )}
+      )} */}
     </nav>
   );
 }
