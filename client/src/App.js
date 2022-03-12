964d1bd325539e327636dcf6aeeaefb2f28d7ea4
chrismuto <chrismuto@gmail.com>
2022-03-12 10:56:43 -0800

    Major formatting improvements to reservations page formatting and hard code

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,42 @@
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import RequireAuth from "./components/RequireAuth";
+import { client } from "./util/apolloClient";
+import { AuthProvider } from "./util/auth";
+import Favorites from "./pages/Favorites";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProtectedPageExample from "./pages/ProtectedPageExample";
 import SignUp from "./pages/SignUp";
-import { client } from "./util/apolloClient";
-import { AuthProvider } from "./util/auth";
-import Favorites from "./pages/Favorites";
 import Reservations from "./pages/Reservations";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <AuthProvider>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            {/* Use <RequiredAuth> for pages that should only be accessible to a
-            user that has logged in.*/}
-            <Route
-              path="/protected"
-              element={
-                <RequireAuth>
-                  <ProtectedPageExample />
-                </RequireAuth>
-              }
-            />
-          </Routes>
-        </AuthProvider>
-      </Router>
-    </ApolloProvider>
+    // <ApolloProvider client={client}>
+    //   <Router>
+    //     <AuthProvider>
+    //       <Navbar />
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/login" element={<Login />} />
+    //         <Route path="/signup" element={<SignUp />} />
+    //         {/* Use <RequiredAuth> for pages that should only be accessible to a
+    //         user that has logged in.*/}
+    //         <Route
+    //           path="/protected"
+    //           element={
+    //             <RequireAuth>
+    //               <ProtectedPageExample />
+    //             </RequireAuth>
+    //           }
+    //         />
+    //       </Routes>
+    //     </AuthProvider>
+    //   </Router>
+    // </ApolloProvider>
+    <Reservations />
   );
 }
 
