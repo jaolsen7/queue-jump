165aa944881ab4fa79acd479bd4979eed42636bc
chrismuto <chrismuto@gmail.com>
2022-03-15 11:05:21 -0700

    made search and home visible to logged in users

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,110 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../util/auth";
-import { NavDropdown } from "react-bootstrap"
+import { NavDropdown } from "react-bootstrap";
 import { useState } from "react";
 import QueueJumpLogo from "./QueueJumpLogo.png";
-import userProfile from "./userProfile.jpg"
+import userProfile from "./userProfile.jpg";
 
 export default function Navbar() {
   const { isLoggedIn, logout } = useAuth();
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   if (!isLoggedIn) {
-  return (
-    <nav className="navbar w-100">
-  <>
-      <img src={QueueJumpLogo} alt={"The logo"} className="col-4" />
-    
-    <NavDropdown  className="col-2" autoClose="outside" title={<img src={userProfile} alt="Profile Menu" onClick={handleShow} className="m-3 col-12" />}>
-        <NavDropdown.Item><Link to="/" className="text-dark text-decoration-none">Home</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to="/search" className="text-dark text-decoration-none">Search</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to="/login" className="text-dark text-decoration-none">Log in</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to="/signup" className="text-dark text-decoration-none">Sign Up</Link></NavDropdown.Item>
-        </NavDropdown>
+    return (
+      <nav className="navbar w-100">
+        <>
+          <img src={QueueJumpLogo} alt={"The logo"} className="col-4" />
+
+          <NavDropdown
+            className="col-2"
+            autoClose="outside"
+            title={
+              <img
+                src={userProfile}
+                alt="Profile Menu"
+                onClick={handleShow}
+                className="m-3 col-12"
+              />
+            }
+          >
+            <NavDropdown.Item>
+              <Link to="/" className="text-dark text-decoration-none">
+                Home
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/search" className="text-dark text-decoration-none">
+                Search
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/login" className="text-dark text-decoration-none">
+                Log in
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/signup" className="text-dark text-decoration-none">
+                Sign Up
+              </Link>
+            </NavDropdown.Item>
+          </NavDropdown>
         </>
-        </nav>
-        )
-        } else {
-          return (
-            <nav className="navbar w-100">
-          <>
-              <img src={QueueJumpLogo} alt={"The logo"} className="col-4" />
-            
-            <NavDropdown  className="col-2" autoClose="outside" title={<img src={userProfile} alt="Profile Menu" onClick={handleShow} className="m-3 col-12" />}>
-        <NavDropdown.Item><Link to="/favorites" className="text-dark text-decoration-none">Favorites</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to="/reservations" className="text-dark text-decoration-none">Reservations</Link></NavDropdown.Item>
-        {/* change logout to use isLoggedIn useAuth */}
-        <NavDropdown.Item><Link to={useAuth} onClick={logout} className="text-dark text-decoration-none">Log Out</Link></NavDropdown.Item>
-      </NavDropdown>
-  </>
-    </nav>
-          )};
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="navbar w-100">
+        <>
+          <img src={QueueJumpLogo} alt={"The logo"} className="col-4" />
+
+          <NavDropdown
+            className="col-2"
+            autoClose="outside"
+            title={
+              <img
+                src={userProfile}
+                alt="Profile Menu"
+                onClick={handleShow}
+                className="m-3 col-12"
+              />
+            }
+          >
+            <NavDropdown.Item>
+              <Link to="/" className="text-dark text-decoration-none">
+                Home
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/search" className="text-dark text-decoration-none">
+                Search
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/favorites" className="text-dark text-decoration-none">
+                Favorites
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link
+                to="/reservations"
+                className="text-dark text-decoration-none"
+              >
+                Reservations
+              </Link>
+            </NavDropdown.Item>
+            {/* change logout to use isLoggedIn useAuth */}
+            <NavDropdown.Item>
+              <Link
+                to={useAuth}
+                onClick={logout}
+                className="text-dark text-decoration-none"
+              >
+                Log Out
+              </Link>
+            </NavDropdown.Item>
+          </NavDropdown>
+        </>
+      </nav>
+    );
+  }
 }
